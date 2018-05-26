f49e9dceead68c5c255145e58e7c36cef4895933
Jacques Begin <jacques.andre.begin@gmail.com>
2018-05-26 02:47:44 -0400

    Adds require for mongoDb queries, removes unused route, add TODO.

--- src/routes/ingredientsRoutes.js
+++ src/routes/ingredientsRoutes.js
@@ -1,19 +1,20 @@
 const express = require("express");
 const router = express.Router();
+const dbQueries = require("../db/mongodbQueries");
 
 module.exports = function(db) {
 
+  
+
   router.get("/", function(req, res) {
     console.log("get all ingredients");
+    //TODO: add logic for accessing mongoDb queries
     res.send("Ingredients Home Page");
-  });
-
-  router.post("/", function(req, res) {
-    console.log("create new ingredient");
   });
 
   router.get("/:id", function(req, res) {
     console.log("get a single ingredient");
+    //TODO: add logic for accessing mongoDb queries
     res.send("Single Ingredient");
   });
 
