0de7cd80c394445f6f38835c071560677450bf00
Jacques Begin <jacques.andre.begin@gmail.com>
2018-05-26 00:08:11 -0400

    Updates express server with links to app routes, app initialization functions, and db startup function.

--- src/server/server.js
+++ src/server/server.js
@@ -1,9 +1,31 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const mongodb = require("mongodb");
-
+const database = require("../db/dbConnections");
+const smoothieRoutes = require("../routes/smoothieRoutes");
+const ingredientsRoutes = require("../routes/ingredientsRoutes");
 
 const PORT = 8882;
 
 var app = express();
 app.use(bodyParser.json());
+
+
+
+startAppServer = () => {
+  var server = app.listen(process.env.PORT || PORT, () => {
+    var port = server.address().port;
+    console.log(`App server running on port ${port}`);
+  });
+}
+
+initializeRoutes = (db) => {
+  app.use("/api/smoothies", smoothieRoutes(db));
+  app.use("/api/ingredients", ingredientsRoutes(db));
+}
+
+// Connect to the mongo database, pass in the app initialization function
+var db = database.mongoDbConnection.connectToServer(startAppServer, initializeRoutes);
+
+
+
+
