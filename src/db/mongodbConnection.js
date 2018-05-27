b88d4c70fb06ee4d5c880ac7de3e8942b5daa47c
Jacques Begin <jacques.andre.begin@gmail.com>
2018-05-26 21:26:46 -0400

    Adds dotenv package, local env variable.

--- src/db/mongodbConnection.js
+++ src/db/mongodbConnection.js
@@ -1,5 +1,5 @@
 const mongodb = require("mongodb");
-const MONGODB_LOCAL = "mongodb://localhost:27017/smoothieDuplicator";
+require('dotenv').config();
 
 var db;
 
@@ -7,21 +7,22 @@ var db;
 module.exports = {
 
   connectToServer: function(startApp, initializeRoutes) {
-    console.log("process.env.MONGODB_URI: ", process.env.MONGODB_URI);
-    mongodb.connect(process.env.MONGODB_URI || MONGODB_LOCAL,
+    mongodb.connect(process.env.MONGODB_URI || process.env.MONGODB_LOCAL,
       { useNewUrlParser: true }, (err, client) => {
-      if (err) {
-        console.log("Database connection error: ", err);
-        // If db connection not established, exit before starting the application
-        process.exit(1);
-      }
+        if (err) {
+          console.log("Database connection error: ", err);
+          // If db connection not established, exit before starting the application
+          process.exit(1);
+        }
 
-      console.log("Connected to mongoDB");
-      db = client.db(process.env.MONGODB_NAME);
-      initializeRoutes(db);
-      startApp();
-      return db;
-    });
+        console.log("Connected to mongoDB");
+        // Must set an environment variable to pass in the name of the db as
+        // local and remote db's may have different names
+        db = client.db(process.env.MONGODB_NAME);
+        initializeRoutes(db);
+        startApp();
+        return db;
+      });
   },
 
   disconnectFromServer: function(db) {
