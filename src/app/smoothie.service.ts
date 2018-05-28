9c51bd45d2039e574e543a58940838b9e4c99531
Jacques Begin <jacques.andre.begin@gmail.com>
2018-05-28 00:22:37 -0400

    Updates smoothie service to use message service, adds get smoothie by id functionality.

--- src/app/smoothie.service.ts
+++ src/app/smoothie.service.ts
@@ -2,15 +2,22 @@ import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { Smoothie } from './classes/smoothie';
 import { Smoothies } from './mock-smoothies';
+import { MessageService } from './message.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SmoothieService {
 
-  constructor() { }
+  constructor(private messageService: MessageService) { }
 
   getSmoothies(): Observable<Smoothie[]> {
+    this.messageService.add('SmoothieService: fetched smoothies');
     return of(Smoothies);
+  }
+
+  getSmoothie(id: number): Observable<Smoothie> {
+    this.messageService.add(`SmoothieService: fetched smoothie id=${id}`);
+    return of(Smoothies.find(smoothie => smoothie.id === id));
   }
 }
