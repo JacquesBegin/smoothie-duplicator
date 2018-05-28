fc028b0ca2967f767124974b1149bec528eb40f1
Jacques Begin <jacques.andre.begin@gmail.com>
2018-05-28 00:20:03 -0400

    Creates a router for navigating through the app.

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { SmoothiesComponent } from './smoothies/smoothies.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { SmoothieDetailComponent } from './smoothie-detail/smoothie-detail.component';
+
+
+
+const routes: Routes = [
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  { path: 'smoothies', component: SmoothiesComponent },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'detail/:id', component: SmoothieDetailComponent }
+]
+
+
+@NgModule({
+  imports: [ RouterModule.forRoot(routes) ],
+  exports: [ RouterModule ]
+})
+
+export class AppRoutingModule { }
