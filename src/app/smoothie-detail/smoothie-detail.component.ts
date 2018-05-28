8621a44f059f49dd22bb9e7596540776c1ff334a
Jacques Begin <jacques.andre.begin@gmail.com>
2018-05-28 00:27:45 -0400

    Modifies smoothie details to use smoothie service to retrieve smoothie data through parameterized route, adds stylings.

--- src/app/smoothie-detail/smoothie-detail.component.ts
+++ src/app/smoothie-detail/smoothie-detail.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+import { SmoothieService } from '../smoothie.service';
 import { Smoothie } from '../classes/smoothie';
 
 @Component({
@@ -10,9 +13,24 @@ export class SmoothieDetailComponent implements OnInit {
 
   @Input() smoothie: Smoothie;
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private smoothieService: SmoothieService,
+    private location: Location
+  ) { }
 
   ngOnInit() {
+    this.getSmoothie();
+  }
+
+  getSmoothie(): void {
+    const id = +this.route.snapshot.paramMap.get('id');
+    this.smoothieService.getSmoothie(id)
+      .subscribe(smoothie => this.smoothie = smoothie);
+  }
+
+  goBack(): void {
+    this.location.back();
   }
 
 }
