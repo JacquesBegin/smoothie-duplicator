d44c26fd1f397f0349f59dd245b2c9d2bcc289a2
Jacques Begin <jacques.andre.begin@gmail.com>
2018-05-29 10:27:09 -0400

    Uses the http client to access smoothie database, adds temporary url to access local database for testing.

--- src/app/smoothie.service.ts
+++ src/app/smoothie.service.ts
@@ -1,23 +1,63 @@
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
+import { catchError, map, tap } from 'rxjs/operators';
 import { Smoothie } from './classes/smoothie';
-import { Smoothies } from './mock-smoothies';
 import { MessageService } from './message.service';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+};
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class SmoothieService {
 
-  constructor(private messageService: MessageService) { }
+  // private smoothiesUrl = '/api/smoothies';
+  private smoothiesUrl = 'http://localhost:8882/api/smoothies';
+
+
+  constructor(
+    private http: HttpClient,
+    private messageService: MessageService) { }
 
   getSmoothies(): Observable<Smoothie[]> {
     this.messageService.add('SmoothieService: fetched smoothies');
-    return of(Smoothies);
+    return this.http.get<Smoothie[]>(this.smoothiesUrl)
+      .pipe(
+        tap(smoothies => this.log('fetched smoothies')),
+        catchError(this.handleError('getSmoothies', []))
+      );
   }
 
-  getSmoothie(id: number): Observable<Smoothie> {
-    this.messageService.add(`SmoothieService: fetched smoothie id=${id}`);
-    return of(Smoothies.find(smoothie => smoothie.id === id));
+  getSmoothie(id: string): Observable<Smoothie> {
+    const url = `${this.smoothiesUrl}/${id}`;
+    return this.http.get<Smoothie>(url).pipe(
+      tap(_ => this.log(`fetched smoothie id=${id}`)),
+      catchError(this.handleError<Smoothie>(`getSmoothie id=${id}`))
+    );
   }
+
+  updateSmoothie(smoothie: any): Observable<any> {
+    return this.http.put(`${this.smoothiesUrl}/${smoothie._id}`, smoothie, httpOptions).pipe(
+      tap(_ => this.log(`updated smoothie id=${smoothie._id}`)),
+      catchError(this.handleError<any>('updateSmoothie'))
+    );
+  }
+
+  private log(message: string) {
+    this.messageService.add('SmoothieService: ' + message);
+  }
+
+  private handleError<T> (operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+      console.error(error);
+      this.log(`${operation} failed: ${error.message}`);
+      return of(result as T);
+    }
+  }
+
 }
