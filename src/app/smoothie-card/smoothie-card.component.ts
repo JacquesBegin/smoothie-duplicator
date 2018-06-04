f13f43833aed9f16cc77dea15c3fa919c1b5e846
Jacques Begin <jacques.andre.begin@gmail.com>
2018-06-04 19:43:55 -0400

    Adds the smoothie card component for smoothie information.

---
+++ src/app/smoothie-card/smoothie-card.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Smoothie } from '../classes/smoothie';
+import { MatCardModule } from '@angular/material/card';
+import { MatChipsModule } from '@angular/material/chips';
+
+@Component({
+  selector: 'app-smoothie-card',
+  templateUrl: './smoothie-card.component.html',
+  styleUrls: ['./smoothie-card.component.css']
+})
+export class SmoothieCardComponent implements OnInit {
+
+  @Input() smoothie: Smoothie;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
